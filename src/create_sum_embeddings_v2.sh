ae26631d857047e526021c508dd5b611ff3e65e7
Exitare <business.exitare@icloud.com>
2024-05-02 09:38:37 -0700

    Create create_sum_embeddings_v2.sh

---
+++ src/create_sum_embeddings_v2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --job-name=embedding_generation
+#SBATCH --time=9-00:00:00
+#SBATCH --partition=exacloud
+#SBATCH --qos=long_jobs
+#SBATCH --ntasks=1
+#SBATCH --mem=64000
+#SBATCH --cpus-per-task=4
+#SBATCH --output=./output_reports/slurm.%N.%j.out
+#SBATCH --error=./error_reports/slurm.%N.%j.err
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=kirchgae@ohsu.edu
+
+total_embeddings=$1
+
+python3 src/create_sum_embeddings_v2.py -l results/  -i 1000000 -e 2
