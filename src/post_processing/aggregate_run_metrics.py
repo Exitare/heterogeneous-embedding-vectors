02fae1ed5e7bc5fa255c85694c9a6394e726df35
Exitare <business.exitare@icloud.com>
2024-05-02 18:20:57 -0700

    Update aggregate_run_metrics.py

--- src/post_processing/aggregate_run_metrics.py
+++ src/post_processing/aggregate_run_metrics.py
@@ -2,11 +2,23 @@ import pandas as pd
 import os, argparse
 from pathlib import Path
 
+save_path = Path("results", "aggregated_metrics")
+
 if __name__ == '__main__':
+    if not save_path.exists():
+        save_path.mkdir(parents=True)
 
     # iterate through results recognizer folder and all its sub folders
     results_path = Path("results", "recognizer")
+    results = []
     for root, dirs, files in os.walk(results_path):
         for file in files:
-            print(file)
+            if file == "metrics.csv":
+                metrics = pd.read_csv(Path(root, file))
+                results.append(metrics)
 
+    # concatenate all metrics
+    results = pd.concat(results)
+
+    # save the concatenated metrics
+    results.to_csv(Path(save_path, "metrics.csv"), index=False)
