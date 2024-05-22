17cfbe6619dcdfd52f72dfe61b76b7918a816c8a
Exitare <business.exitare@icloud.com>
2024-05-21 19:18:38 -0700

    Update aggregate_run_metrics.py
    - Add argument parser to accommodate new single and multi setup

--- src/post_processing/aggregate_run_metrics.py
+++ src/post_processing/aggregate_run_metrics.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import os, argparse
 from pathlib import Path
-
-save_path = Path("results", "aggregated_metrics")
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
+    parser = ArgumentParser(description='Aggregate metrics from recognizer results')
+    parser.add_argument("--data_folder", "-d", type=Path, required=True,
+                        help="Folder containing the recognizer results")
+    parser.add_argument("--output", "-o", type=Path, required=True, help="Folder to save the aggregated metrics")
+    args = parser.parse_args()
+
+    data_folder: Path = args.data_folder
+    save_path: Path = args.output
+
     if not save_path.exists():
         save_path.mkdir(parents=True)
 
     # iterate through results recognizer folder and all its sub folders
-    results_path = Path("results", "recognizer")
     results = []
-    for root, dirs, files in os.walk(results_path):
+    for root, dirs, files in os.walk(data_folder):
         for file in files:
             if file == "metrics.csv":
                 print("Processing", Path(root, file))
