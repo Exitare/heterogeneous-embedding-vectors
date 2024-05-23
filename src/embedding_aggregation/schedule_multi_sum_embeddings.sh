445c6bf35d4198cfc54216e4691ed89090a9b130
Exitare <business.exitare@icloud.com>
2024-05-23 11:58:32 -0700

    Update schedule_multi_sum_embeddings.sh
    - Add missing sbatch

--- src/embedding_aggregation/schedule_multi_sum_embeddings.sh
+++ src/embedding_aggregation/schedule_multi_sum_embeddings.sh
@@ -5,5 +5,5 @@ cancer_types=$1
 # iterate through 2 to 10
 for i in $(seq 2 10)
 do
-  ./src/embedding_aggregation/create_multi_sum_embeddings.sh $i "${cancer_types}"
+  sbatch ./src/embedding_aggregation/create_multi_sum_embeddings.sh $i "${cancer_types}"
 done
