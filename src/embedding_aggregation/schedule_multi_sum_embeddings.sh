1d0b6089116fab703ce51654170ebfcbb969a98f
Exitare <business.exitare@icloud.com>
2024-05-23 11:34:00 -0700

    Update schedule_multi_sum_embeddings.sh
    - Fix typo

--- src/embedding_aggregation/schedule_multi_sum_embeddings.sh
+++ src/embedding_aggregation/schedule_multi_sum_embeddings.sh
@@ -6,5 +6,5 @@ echo $cancer_types
 # iterate through 2 to 10
 for i in $(seq 2 10)
 do
-  sbatch ./src/embedding_aggregation/create_multi_sum_embedding.sh $i $cancer_type
+  sbatch ./src/embedding_aggregation/create_multi_sum_embeddings.sh $i $cancer_type
 done
