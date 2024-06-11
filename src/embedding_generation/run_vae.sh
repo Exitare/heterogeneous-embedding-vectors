4f061b1d970e5295c0f7861efca223b87b0b232f
Exitare <business.exitare@icloud.com>
2024-06-11 09:18:17 -0700

    Update run_vae.sh
    - Increase number of cpus

--- src/embedding_generation/run_vae.sh
+++ src/embedding_generation/run_vae.sh
@@ -6,7 +6,7 @@
 #SBATCH --qos=long_jobs
 #SBATCH --ntasks=1
 #SBATCH --mem=64000
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=64
 #SBATCH --output=./output_reports/slurm.%N.%j.out
 #SBATCH --error=./error_reports/slurm.%N.%j.err
 #SBATCH --mail-type=FAIL
