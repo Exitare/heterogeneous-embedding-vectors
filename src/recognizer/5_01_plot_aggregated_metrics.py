fb6b0f600a2e42a3c9ff03acb50782dda7a1e91e
Exitare <business.exitare@icloud.com>
2024-08-23 12:00:24 -0700

    Update 5_01_plot_aggregated_metrics.py

--- src/recognizer/5_01_plot_aggregated_metrics.py
+++ src/recognizer/5_01_plot_aggregated_metrics.py
@@ -4,27 +4,49 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from argparse import ArgumentParser
 
-save_folder = Path("figures", "performance")
+save_folder = Path("figures", "recognizer")
+load_folder = Path("results", "recognizer", "aggregated_metrics")
 
 if not save_folder.exists():
     save_folder.mkdir(parents=True)
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Aggregate metrics from recognizer results')
-    parser.add_argument("--data", "-d", type=Path, required=True,
-                        help="Data containing the recognizer results")
     parser.add_argument("--file_name", "-fn", type=Path, required=False, help="File name to save the plot")
     parser.add_argument("-c", "--cancer", required=False, nargs='+')
     parser.add_argument("--foundation", "-f", action="store_true", help="Plot for foundation model")
+    parser.add_argument("--multi", "-m", action="store_true", help="Plot for multi recognizer")
 
     args = parser.parse_args()
-    data_folder: Path = args.data
+    multi = args.multi
     file_name: Path = args.file_name
-    cancer: [] = args.cancer
+    cancers: [] = args.cancer
     foundation: bool = args.foundation
 
-    print(cancer)
-    df = pd.read_csv(data_folder)
+    selected_cancers = '_'.join(cancers)
+
+    print(f"Loading data for multi: {multi}, cancers: {cancers}, foundation: {foundation}")
+
+    if multi:
+        if foundation:
+            load_folder = Path(load_folder, "multi_recognizer_foundation", selected_cancers)
+            file = Path(load_folder, "split_metrics.csv")
+        else:
+            load_folder = Path(load_folder, "multi_recognizer", selected_cancers)
+            file = Path(load_folder, "metrics.csv")
+    else:
+        if foundation:
+            load_folder = Path(load_folder, "single_recognizer_foundation")
+            file = Path(load_folder, "metrics.csv")
+        else:
+            load_folder = Path(load_folder, "single_recognizer")
+            file = Path(load_folder, "metrics.csv")
+
+
+    print(f"Loading file {file}...")
+    df = pd.read_csv(file)
+
+
 
     # calculate mean of embeddings
     df = df.groupby(["embeddings", "embedding"]).mean(numeric_only=True)
@@ -37,29 +59,36 @@ if __name__ == '__main__':
 
     # print mean accuracy for each embedding
     print(df[["embedding", "accuracy"]].groupby("embedding").mean(numeric_only=True))
+    df_mean = df.groupby("embeddings", as_index=False)["accuracy"].mean()
 
     # upper case all embedding
     df["embedding"] = df["embedding"].str.upper()
 
     title = ''
 
-    if cancer is not None:
-        title = f"Mean accuracy of embeddings using cancer {' '.join([can for can in cancer])}"
+    if cancers is not None:
+        title = f"Mean accuracy of walk distances using cancer\n{' '.join([can for can in cancers])}"
     else:
-        title = "Mean accuracy of embeddings"
+        title = "Mean accuracy of walk distances"
 
-
-
-    # plot
+    # Plot
     fig = plt.figure(figsize=(10, 5), dpi=150)
     sns.set_theme(style="whitegrid")
     sns.set_context("paper")
-    sns.lineplot(data=df, x="embeddings", y="accuracy", hue="embedding")
+
+    # Plot individual embeddings
+    sns.lineplot(data=df, x="embeddings", y="accuracy", hue="embedding", palette="tab10", alpha=0.6)
+
+    # Plot mean line
+    sns.lineplot(data=df_mean, x="embeddings", y="accuracy", color='black', marker='o', linestyle='--', label='Mean')
+
     plt.title(title)
     plt.ylabel("Accuracy")
-    plt.xlabel("Embedding")
+    plt.xlabel("Walk Distance")
     plt.xticks(rotation=45)
+    plt.legend(title="Embedding")
     plt.tight_layout()
+
     if file_name is None:
         plt.show()
     else:
