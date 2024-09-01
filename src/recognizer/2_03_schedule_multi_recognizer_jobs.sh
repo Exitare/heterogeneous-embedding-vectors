bfff2129ba0e97a048de7b09b9864ad578945c65
Exitare <business.exitare@icloud.com>
2024-08-31 17:48:28 -0700

    Update 2_03_schedule_multi_recognizer_jobs.sh

--- src/recognizer/2_03_schedule_multi_recognizer_jobs.sh
+++ src/recognizer/2_03_schedule_multi_recognizer_jobs.sh
@@ -15,11 +15,11 @@ then
   upper_walk_distance=10
 fi
 
-for i in $(seq 2 $upper_walk_distance)
+for walk_distance in $(seq 2 $upper_walk_distance)
 do
   # run it 30 times
-  for j in $(seq 1 30)
+  for iteration in $(seq 1 30)
   do
-    sbatch ./src/recognizer/2_03_run_multi_recognizer.sh $i $j "${cancer_types}"
+    sbatch ./src/recognizer/2_03_run_multi_recognizer.sh $walk_distance $iteration "${cancer_types}"
   done
 done
