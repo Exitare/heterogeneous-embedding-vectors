1c81a1233aaa057184fd14144f9333bbcd48ecbd
Exitare <business.exitare@icloud.com>
2025-02-01 09:07:13 -0800

    Update h5 scripts

--- src/data_preparation/0_14_create_h5_v2.sh
+++ src/data_preparation/0_14_create_h5_v2.sh
@@ -5,12 +5,11 @@
 #SBATCH --partition=batch
 #SBATCH --qos=long_jobs
 #SBATCH --ntasks=1
-#SBATCH --mem=64000
+#SBATCH --mem=512000
 #SBATCH --cpus-per-task=2
 #SBATCH --output=./output_reports/slurm.%N.%j.out
 #SBATCH --error=./error_reports/slurm.%N.%j.err
 #SBATCH --mail-type=FAIL,BEGIN,END
 #SBATCH --mail-user=kirchgae@ohsu.edu
 
-
-python3 src/data_preparation/0_14_create_h5.py -c BRCA LUAD STAD BLCA COAD THCA
+python3 src/data_preparation/0_14_create_h5_v2.py -c BRCA LUAD STAD BLCA COAD THCA
--- src/data_preparation/0_15_create_h5_classifier_v2.sh
+++ src/data_preparation/0_15_create_h5_classifier_v2.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --job-name=c_h5df
+#SBATCH --job-name=h5_classifier
 #SBATCH --time=9-00:00:00
 #SBATCH --partition=batch
 #SBATCH --qos=long_jobs
 #SBATCH --ntasks=1
-#SBATCH --mem=64000
+#SBATCH --mem=512000
 #SBATCH --cpus-per-task=2
 #SBATCH --output=./output_reports/slurm.%N.%j.out
 #SBATCH --error=./error_reports/slurm.%N.%j.err
 #SBATCH --mail-type=FAIL,BEGIN,END
 #SBATCH --mail-user=kirchgae@ohsu.edu
 
-
-python3 src/data_preparation/0_14_create_h5.py -c BRCA LUAD STAD BLCA COAD THCA
+python3 src/data_preparation/0_14_create_h5_classifier_v2.py -c BRCA LUAD STAD BLCA COAD THCA
