5e63bc603c894436b69a6ca6a1cefeeea2bd3fc3
Exitare <business.exitare@icloud.com>
2025-02-04 14:43:00 -0800

    Add new schedule scripts

---
+++ src/recognizer/models/2_02_schedule_simple_recognizer_foundation_noise_jobs.sh
@@ -0,0 +1,41 @@
+#./src/recognizer/models/2_02_schedule_simple_recognizer_foundation_jobs.sh "BRCA LUAD STAD BLCA COAD THCA" 15000 10
+
+cancer_types=$1
+amount_of_summed_embeddings=$2
+# upper bound is the maximum walk distance
+upper_bound=$3
+
+
+# if cancer_types is not provided, then exit
+if [ -z "$cancer_types" ]; then
+  echo "No cancer types provided!"
+  exit 1
+fi
+
+# if upper bound not set, set to 15
+if [ -z "$upper_bound" ]
+then
+  echo "Upper bound not set, setting to 15"
+  upper_bound=15
+fi
+
+# if summed embeddings count not set, set to 100000
+if [ -z "$amount_of_summed_embeddings" ]
+then
+  echo "Amount of summed embeddings count not set, set to 100000."
+  amount_of_summed_embeddings=100000
+fi
+
+echo "Upper bound: $upper_bound"
+echo "Amount of summed embeddings: $amount_of_summed_embeddings"
+echo "cancer types: $cancer_types"
+
+
+for noise in 0.1 0.2 0.3 0.4 0.5 0.6
+do
+    # run it 30 times
+    for iteration in $(seq 1 30)
+    do
+      ./src/recognizer/models/2_01_run_simple_recognizer.sh "${cancer_types}" -1 $iteration $amount_of_summed_embeddings $noise
+    done
+done
---
+++ src/recognizer/models/2_04_schedule_multi_recognizer_foundation_noise_jobs.sh
@@ -0,0 +1,45 @@
+
+cancer_types=$1
+amount_of_summed_embeddings=$2
+# upper bound is the maximum walk distance
+upper_bound=$3
+local=$4
+
+
+
+# if upper bound not set, set to 10
+if [ -z "$upper_bound" ]
+then
+  echo "Upper bound not set, setting to 10"
+  upper_bound=10
+fi
+
+# if summed embeddings count not set, set to 100000
+if [ -z "$amount_of_summed_embeddings" ]
+then
+  echo "Amount of summed embeddings count not set, setting to 100000."
+  amount_of_summed_embeddings=100000
+fi
+
+if [ -z "$local" ]
+then
+  echo "Running on cluster"
+else
+  echo "Running locally"
+fi
+
+
+for noise in 0.1 0.2 0.3 0.4 0.5 0.6
+do
+  # run it 30 times
+  for iteration in $(seq 1 30)
+  do
+    #if local not provided use sbatch
+    if [ -z "$local" ]
+    then
+        sbatch ./src/recognizer/models/2_03_run_multi_recognizer.sh -1 $iteration "${cancer_types}" $amount_of_summed_embeddings $noise
+    else
+        ./src/recognizer/models/2_03_run_multi_recognizer.sh -1 $iteration "${cancer_types}" $amount_of_summed_embeddings $noise
+    fi
+  done
+done
